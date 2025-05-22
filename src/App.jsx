3b8276fc72e7a7bd569209cb2e00fc7bc907a722
gulatipalak <gulatipalak2397@gmail.com>
2025-05-22 22:09:32 +0530

    theme setup : colors, font-family

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="bg-primary font-candal text-3xl font-bold underline">
         Hello world!
       </h1>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+       fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        candal: ['Candal', 'sans-serif'],
+      },
+      colors: {
+        // Custom colors
+        primary: '#2B1354',       
+        secondary: '#3E216E',     
+        green : '#C4CD24',
+        black: '#2B282F',
+        brand:{
+          white: '#EBEBEB',
+        },
+      },
+    },
   },
   plugins: [],
 }
